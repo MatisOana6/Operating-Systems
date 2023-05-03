6c6e856f2aefd32e167db058408d184bfc2f7370
Matis Oana <matis.oana@yahoo.com>
2023-05-04 02:44:14 +0300

    last1

--- a2/a2.c
+++ a2/a2.c
@@ -4,60 +4,107 @@
 #include <sys/wait.h>
 #include <pthread.h>
 #include "a2_helper.h"
+#include <semaphore.h>
+#include <fcntl.h>
 
-#define NR_THREADS 39
+#define NR_THREADS_7 39
+#define NR_THREADS_5 5
+#define NR_THREADS_3 6
 
-typedef struct{
-    int *array;
-    int start;
-    int end;
-}Thread_struct;
+sem_t *sem_p5_2;
+sem_t *sem_p5_3;
 
-void *thread_function1(void *arg)
+pthread_mutex_t lock_process5_3_2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_process5_2_3 = PTHREAD_MUTEX_INITIALIZER;
+int process_5_t2 = 0;
+int process_5_t3 = 0;
+
+typedef struct
 {
-    info(BEGIN,5,1);
-    info(BEGIN,5,3);
-    info(BEGIN,5,2);
-    info(BEGIN,5,4);
-    info(BEGIN,5,5);
+    int thread_number;
+    int process_number;
 
-    info(END,5,5);
-    info(END,5,4);
-    info(END,5,2);
-    info(END,5,3);
-    info(END,5,1);
-    return NULL;
+} Thread_struct;
+
+void *thread_function(void *arg);
+void create_threads(int process_nb, int thread_nb)
+{
+    pthread_t threads[thread_nb];
+    Thread_struct thread_vector[thread_nb];
+
+    for (int i = 0; i < thread_nb; i++)
+    {
+        thread_vector[i] = (Thread_struct){
+            .process_number = process_nb,
+            .thread_number = i + 1,
+        };
+        pthread_create(&threads[i], NULL, thread_function, &thread_vector[i]);
+    }
+
+    for (int i = 0; i < thread_nb; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 }
 
-void *thread_function3(void *arg)
+void *thread_function(void *arg)
 {
-    info(BEGIN,3,1);
-    info(BEGIN,3,2);
-    info(BEGIN,3,3);
-    info(BEGIN,3,4);
-    info(BEGIN,3,5);
-    info(BEGIN,3,6);
+    Thread_struct *t_struct = (Thread_struct *)arg;
+    int process_id = t_struct->process_number;
+    int thread_id = t_struct->thread_number;
 
-    info(END,3,6);
-    info(END,3,5);
-    info(END,3,4);
-    info(END,3,3);
-    info(END,3,2);
-    info(END,3,1);
+    if (process_id != 5)
+    {
+       info(BEGIN, process_id, thread_id);
+       info(END, process_id, thread_id);
+    }
+    else if (thread_id == 2 || thread_id == 3)
+    {
+        if (thread_id == 3)
+        {
+            info(BEGIN, process_id, thread_id);
+            sem_post(sem_p5_3);
+            sem_wait(sem_p5_2);
+            info(END, process_id, thread_id);
+        }
+        else if (thread_id == 2)
+        {
+            sem_wait(sem_p5_3);
+            info(BEGIN, process_id, thread_id);
+            info(END, process_id, thread_id);
+            sem_post(sem_p5_2);
+        }
+    }
+    else if(process_id != 3)
+    {
+        info(BEGIN, process_id, thread_id);
+        info(END, process_id, thread_id);
+    }
+    else
+    {
+        info(BEGIN, process_id, thread_id);
+        info(END, process_id, thread_id);
+    }
 
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
-    pid_t pid2=-1;
-    pid_t pid3=-1; 
-    pid_t pid4=-1;
-    pid_t pid5=-1;
-    pid_t pid6=-1;
-    pid_t pid7=-1;
-    pid_t pid8=-1;
-    
+    sem_unlink("sem_p5_2");
+    sem_unlink("sem_p5_3");
+
+    sem_p5_2 = sem_open("semaphore_proc_5_2", O_CREAT, 0644, 0);
+    sem_p5_3 = sem_open("semaphore_proc_5_3", O_CREAT, 0644, 0);
+
+    pid_t pid2 = -1;
+    pid_t pid3 = -1;
+    pid_t pid4 = -1;
+    pid_t pid5 = -1;
+    pid_t pid6 = -1;
+    pid_t pid7 = -1;
+    pid_t pid8 = -1;
+
     init();
     info(BEGIN, 1, 0);
 
@@ -71,6 +118,7 @@ int main(int argc, char **argv)
     {
         info(BEGIN, 2, 0);
         pid3 = fork();
+        create_threads(3,6);
         if (pid3 == -1)
         {
             perror("Nu se poate crea proces");
@@ -79,9 +127,6 @@ int main(int argc, char **argv)
         else if (pid3 == 0)
         {
             info(BEGIN, 3, 0);
-            pthread_t thread;
-            pthread_create(&thread,NULL,thread_function3,NULL);
-            pthread_join(thread,NULL);
             info(END, 3, 0);
             return 0;
         }
@@ -107,9 +152,6 @@ int main(int argc, char **argv)
                 else if (pid7 == 0)
                 {
                     info(BEGIN, 7, 0);
-                   // pthread_t tids[NR_THREADS];
-                   // pthread_create(&tids,)
-                  
                     info(END, 7, 0);
                     return 0;
                 }
@@ -131,8 +173,9 @@ int main(int argc, char **argv)
     else
     {
         waitpid(pid2, NULL, 0);
-
+        create_threads(5, 5);
         pid5 = fork();
+
         if (pid5 == -1)
         {
             perror("Nu se poate crea proces");
@@ -141,8 +184,7 @@ int main(int argc, char **argv)
         else if (pid5 == 0)
         {
             info(BEGIN, 5, 0);
-            pthread_t tid;
-            pthread_create(&tid, NULL, thread_function1, NULL);
+
             pid6 = fork();
             if (pid6 == -1)
             {
@@ -175,7 +217,6 @@ int main(int argc, char **argv)
             else
             {
                 waitpid(pid6, NULL, 0);
-                pthread_join(tid, NULL);
                 info(END, 5, 0);
                 return 0;
             }
